77c582926582dd58462858673453246190ea140a
renolulic94 <reno.lulic94@gmail.com>
2022-06-12 12:52:35 +0200

    First

---
+++ MetadataRefresherForOpensea.py
@@ -0,0 +1,58 @@
+'''
+By: Renato Lulic
+Instagram: renato_lulic
+Date: June 12th, 2022
+
+For instructions or information, please refer to README.md
+'''
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+# To wait for side load
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Allows us to use keyboard keys
+from selenium.webdriver.common.keys import Keys
+import time
+
+# NFT contract address
+contractAddress = "0xc5df71db9055e6e1d9a37a86411fd6189ca2dbbb"
+
+# Start refreshing metadata from x Nft
+startFrom = 1528
+
+# Up-to which number of Nfts do you want to keep refreshing?
+refreshToNumber = 2300
+
+
+# Run in the background? True/False
+runInBackground = False
+
+
+
+# ---------------------------------------- Do not change anything below this comment----------------------------------------------------------
+options = Options()
+options.headless = runInBackground
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
+
+for tokenId in range(startFrom, refreshToNumber):
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    url = "https://opensea.io/assets/matic/{}/{}".format(contractAddress, tokenId)
+    driver.get(url)
+    try:
+        try:
+            refreshButton = driver.find_element_by_xpath('./html/body/div[1]/div/main/div/div/div/div[2]/div/section[1]/div/div[2]/div/button[1]')
+            refreshButton.click()
+            print("SUCCESS! Refreshing")
+        except:
+            print("ERROR! Could not refresh!")
+    except:
+        print("An error has occurred")
+        time.sleep(0)
+        driver.quit()
+else:
+     print("Refreshing finished!")
---
+++ README.md
@@ -0,0 +1,14 @@
+# Opensea Mass Metadata Refresh Bot
+
+A simple selenium bot that can refresh Metadata for entire collection on Opensea.
+
+Note: This was a fun project for experimental and learning purposes. 
+</br>
+
+# Installation and Instructions
+1. Download the code from this repository
+
+2. install the following module
+```
+pip install selenium
+```
